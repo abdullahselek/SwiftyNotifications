27c4f6467d77241bfce04d1ca3fae775d0f68e3b
abdullahselek <abdullahselek@gmail.com>
2017-03-27 19:45:34 +0200

    - create project

---
+++ SwiftyNotifications/SwiftyNotifications.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyNotifications.h
+//  SwiftyNotifications
+//
+//  Created by Abdullah Selek on 27/03/2017.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyNotifications.
+FOUNDATION_EXPORT double SwiftyNotificationsVersionNumber;
+
+//! Project version string for SwiftyNotifications.
+FOUNDATION_EXPORT const unsigned char SwiftyNotificationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyNotifications/PublicHeader.h>
+
+
