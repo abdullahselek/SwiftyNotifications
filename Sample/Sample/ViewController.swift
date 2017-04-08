cde0265cde02405b69ea7c3d13a8c56e5e8d138b
abdullahselek <abdullahselek@gmail.com>
2017-04-08 12:39:33 +0200

    add show & dismiss buttons

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -11,13 +11,23 @@ import SwiftyNotifications
 
 class ViewController: UIViewController {
 
+    private var notification: SwiftyNotifications!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let notification = SwiftyNotifications.withStyle(style: .info,
-                                                         title: "Information Title",
-                                                         subtitle: "Information description can be anything to give a description")
+        notification = SwiftyNotifications.withStyle(style: .info,
+                                                     title: "Information Title",
+                                                     subtitle: "Information description can be anything to give a description")
         view.addSubview(notification)
+    }
+
+    @IBAction func show(_ sender: Any) {
+        notification.show()
+    }
+
+    @IBAction func dismiss(_ sender: Any) {
+        notification.dismiss()
     }
 
     override func didReceiveMemoryWarning() {
