8f7b74a0c34680eb2dc5096940c7ad53ba43cf58
abdullahselek <abdullahselek@gmail.com>
2017-04-08 19:33:49 +0200

    show different notifications

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -12,6 +12,7 @@ import SwiftyNotifications
 class ViewController: UIViewController {
 
     private var notification: SwiftyNotifications!
+    private var customNotification: SwiftyNotifications!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +28,47 @@ class ViewController: UIViewController {
     }
 
     @IBAction func dismiss(_ sender: Any) {
+        if customNotification != nil {
+            customNotification.dismiss()
+        }
         notification.dismiss()
+    }
+
+    @IBAction func changeStyle(_ sender: Any) {
+        let style = (sender as! UIButton).tag
+        switch style {
+        case 0:
+            notification.customize(style: .normal)
+            break
+        case 1:
+            notification.customize(style: .error)
+            break
+        case 2:
+            notification.customize(style: .success)
+            break
+        case 3:
+            notification.customize(style: .info)
+            break
+        case 4:
+            notification.customize(style: .warning)
+            break
+        case 5:
+            notification.dismiss()
+            customNotification = SwiftyNotifications.withStyle(style: .custom, title: "Custom", subtitle: "Custom notification with custom image and colors")
+            customNotification.leftAccessoryView.image = UIImage(named: "apple_logo")!
+            customNotification.setCustomColors(backgroundColor: UIColor.cyan, textColor: UIColor.white)
+            view.addSubview(customNotification)
+            customNotification.show()
+            break
+//        case 6:
+//            notification.customize(style: .blurDark)
+//            break
+//        case 7:
+//            notification.customize(style: .blurLight)
+//            break
+        default:
+            break
+        }
     }
 
     override func didReceiveMemoryWarning() {
