825d8b0a9cb0524687b57a465f3439b957e8a84a
Abdullah Selek <abdullahselek@gmail.com>
2017-09-13 16:14:43 +0200

    create SwiftyNotificationMessage for just displaying message

---
+++ SwiftyNotificationsTests/SwiftyNotificationsMessageTests.swift
@@ -0,0 +1,53 @@
+//
+//  SwiftyNotificationsMessageTests.swift
+//  SwiftyNotifications
+//
+//  Created by Selek, Abdullah on 13.09.17.
+//  Copyright © 2017 Abdullah Selek. All rights reserved.
+//
+
+import XCTest
+
+@testable import SwiftyNotifications
+
+class SwiftyNotificationsMessageTests: XCTestCase {
+
+    var swiftyNotificationsMessage: SwiftyNotificationsMessage!
+
+    override func setUp() {
+        swiftyNotificationsMessage = SwiftyNotificationsMessage.instanceFromNib()
+        swiftyNotificationsMessage.direction = .top
+    }
+
+    func testInstanceFromNib() {
+        XCTAssertNotNil(swiftyNotificationsMessage)
+        XCTAssertNotNil(swiftyNotificationsMessage.messageLabel)
+    }
+
+    func testInitWithThreeParameters() {
+        let swiftyNotificationsMessage = SwiftyNotificationsMessage.withBackgroundColor(color: UIColor.black,
+                                                                                        message: "message",
+                                                                                        direction: .top)
+        XCTAssertNotNil(swiftyNotificationsMessage)
+    }
+
+    func testInitWithFourParameters() {
+        let swiftyNotificationsMessage = SwiftyNotificationsMessage.withBackgroundColor(color: UIColor.black,
+                                                                                        message: "message",
+                                                                                        dismissDelay: 1.0,
+                                                                                        direction: .bottom)
+        XCTAssertNotNil(swiftyNotificationsMessage)
+    }
+
+    func testInitWithFiveParameters() {
+        let swiftyNotificationsMessage = SwiftyNotificationsMessage.withBackgroundColor(color: UIColor.black,
+                                                                                        message: "message",
+                                                                                        dismissDelay: 1.0,
+                                                                                        direction: .top,
+                                                                                        touchHandler: {
+                                                                                            
+        })
+        XCTAssertNotNil(swiftyNotificationsMessage)
+    }
+
+}
